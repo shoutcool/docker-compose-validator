be7b4fdbd09147ddb8f05b29a6e9974e27eaa92d
Thomas Repnik <shout@gmx.ch>
2016-08-20 15:29:34 +0200

    refactored v1 tests to parameterized junit tests where possible

---
+++ src/test/java/ch/smartclue/docker/validation/ParametrizedValidatorV1Test.java
@@ -0,0 +1,95 @@
+package ch.smartclue.docker.validation;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.mockito.ArgumentCaptor;
+
+import ch.smartclue.docker.exception.DockerComposeValidationException;
+
+@RunWith(Parameterized.class)
+public class ParametrizedValidatorV1Test {
+
+	@Parameter(0)
+	public String content;
+
+	@Parameter(1)
+	public Class<? extends Exception> expectedException;
+	
+	@Parameter(2)
+	public String expectedExceptionMsg;
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	private static ValidatorManager validatorManager = new ValidatorManager();
+	
+	@BeforeClass
+	public static void initClass(){
+		List<ValidatorInstance> instances = ReflectionUtil.createValidationInstancesFromPackageName("ch.smartclue.docker.yml.common");
+		instances.addAll(ReflectionUtil.createValidationInstancesFromPackageName("ch.smartclue.docker.yml.v1"));
+		for (ValidatorInstance instance : instances){
+			validatorManager.addValidatorInstance(instance);
+		}
+	}
+	
+	@Parameters(name = "{index}: {0} throws {1}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+        	{createNamedString("build"), null, null},
+        	{"build: .\nimage: .", DockerComposeValidationException.class, "not allowed to use 'build' and 'image' together"},
+        	{"dockerfile: .", DockerComposeValidationException.class, "'/build' must be specified"},
+        	{"dockerfile: .\nimage: .", DockerComposeValidationException.class, "not allowed to use 'dockerfile' and 'image' together"},
+        	{createNamedList("cap_add"), null, null},
+            {createNamedString("log_driver"), null, null} 
+        });
+    }
+	
+	@Test
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void testValidator() throws Exception {
+	    //setup expected exception
+	    if (expectedException != null) {
+	        thrown.expect(expectedException);
+	        thrown.expectMessage(expectedExceptionMsg);
+	    }
+	    
+	    Validator testee = new ValidatorV1Impl(content, validatorManager);
+	    AbstractValidatorImpl spiedTestee = spy((AbstractValidatorImpl)testee);
+	    spiedTestee.validate();
+	    
+	    if (expectedException != null) {
+	    	ArgumentCaptor<List> captor = ArgumentCaptor.forClass(List.class);
+	    	verify(spiedTestee).executeValidators(captor.capture(), anyObject());
+	    		assertTrue(captor.getValue().size() > 0);
+	    }
+	}
+		
+	
+	
+	private static String createNamedList(String name){
+		return String.format("%s:\n   - dummyListEntry", name);
+	}
+	
+	private static String createNamedMap(String name){
+		return String.format("%s:\n   dummyKey: dummyValue", name);
+	}
+	
+	private static String createNamedString(String name){
+		return String.format("%s: dummyString", name);
+	}
+
+}
--- src/test/java/ch/smartclue/docker/validation/ValidatorV1Test.java
+++ src/test/java/ch/smartclue/docker/validation/ValidatorV1Test.java
@@ -26,65 +26,6 @@ public class ValidatorV1Test {
 	}
 	
 	@Test
-	public void build_validString_successful() throws Exception {
-		String content = "build: test";
-		Validator testee = new ValidatorV1Impl(content, validatorManager);
-		testee.validate();
-	}
-	
-	@Test
-	public void build_emptyString_throwsException() throws Exception {
-		expectedException.expect(DockerComposeValidationException.class);
-		expectedException.expectMessage("must not be empty");
-		String content = "build:";
-		Validator testee = new ValidatorV1Impl(content, validatorManager);
-		testee.validate();
-	}
-	
-	@Test
-	public void build_withImage_throwsException() throws Exception {
-		expectedException.expect(DockerComposeValidationException.class);
-		expectedException.expectMessage("not allowed to use 'build' and 'image' together");
-		String content = "build: .\nimage: .";
-		Validator testee = new ValidatorV1Impl(content, validatorManager);
-		testee.validate();
-	}
-	
-	@Test
-	public void dockerfile_missingBuild_throwsException() throws Exception {
-		expectedException.expect(DockerComposeValidationException.class);
-		expectedException.expectMessage("'/build' must be specified");
-		String content = "dockerfile: .";
-		Validator testee = new ValidatorV1Impl(content, validatorManager);
-		testee.validate();
-	}
-	
-	@Test
-	public void dockerfile_withImage_throwsException() throws Exception {
-		expectedException.expect(DockerComposeValidationException.class);
-		expectedException.expectMessage("not allowed to use 'dockerfile' and 'image' together");
-		String content = "dockerfile: .\nimage: .";
-		Validator testee = new ValidatorV1Impl(content, validatorManager);
-		testee.validate();
-	}
-	
-	@Test
-	public void capAdd_validList_successful() throws Exception {
-		String content = "cap_add:\n   - ALL";
-		Validator testee = new ValidatorV1Impl(content, validatorManager);
-		testee.validate();
-	}
-
-	@Test
-	public void capAdd_asString_throwsException() throws Exception {
-		expectedException.expect(ClassCastException.class);
-		String content = "cap_add: .";
-		Validator testee = new ValidatorV1Impl(content, validatorManager);
-		testee.validate();
-	}
-	
-	
-	@Test
 	public void customValidator_image_throwsException() throws Exception{
 		expectedException.expect(DockerComposeValidationException.class);
 		expectedException.expectMessage("must not be 'foo'");
@@ -105,6 +46,8 @@ public class ValidatorV1Test {
 		testee.addAdditionalValidatorsByClass(CustomImageValidatorWithoutAnnotation.class);
 		testee.validate(content);
 	}
+	
+
 }
 
 
